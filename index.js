4c09191cc08a18e835b6ec73413d874a7302678b
Novikov Eugene <evgeniy.novikov@neuro-city.ru>
2023-10-02 16:39:50 +0300

    Создание сервера. Nodemon. Вывод HTML-страницы. Обработка POST-запроса.

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-const yargs = require('yargs')
-const pcg = require('./package.json')
-const {addNote, printNotes, removeNote} = require('./notes.controller')
+const http = require('http')
+const chalk = require('chalk')
+const fs = require('fs/promises')
+const path = require('path')
+const {addNote} = require('./notes.controller')
 
-yargs.version(pcg.version)
+const port = 3000
 
-yargs.command({
-    command: 'add',
-    describe: 'Add new note to list',
-    builder: {
-        title: {
-            type: 'string',
-            describe: 'Note title',
-            demandOption: true,
-        }
-    },
-    handler({title}) {
-        addNote(title)
+const basePath = path.join(__dirname, 'pages')
+
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET') {
+        const content = await fs.readFile(path.join(basePath, 'index.html'))
+        // res.setHeader('Content-Type', 'text/html')
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        })
+        res.end(content)
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        })
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const title = body.toString().split('=')[1].replaceAll('+', ' ')
+            addNote(title)
+
+            res.end(`Title = ${title}`)
+        })
     }
 })
 
-yargs.command({
-    command: 'remove',
-    describe: 'Remove note by id',
-    builder: {
-        id: {
-            type: 'string',
-            describe: 'Note id',
-            demandOption: true,
-        }
-    },
-    async handler({id}) {
-        await removeNote(id)
-    }
+server.listen(port, () => {
+    console.log(chalk.green(`Server has been started on port ${port}`))
 })
-
-yargs.command({
-    command: 'list',
-    describe: 'Print all notes',
-    async handler() {
-        await printNotes()
-    }
-})
-
-yargs.parse()
